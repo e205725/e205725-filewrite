09708e48fc281d08d41797c8a1fcc19e1db989df
Namiki Teruya <e205725@ie.u-ryukyu.ac.jp>
2022-10-14 21:18:30 +0900

    second

--- fileWrite/fileWrite_test.go
+++ fileWrite/fileWrite_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestFileWrite(t *testing.T) {
 	result := Hello("Namiki")
-	want := "Hi, Namiki. Welcome!"
+	want := "Hello, Namiki. Welcome!"
 	if result != want {
 		t.Errorf("fileWrite.Hello() = %q want %q", result, want)
 	}
